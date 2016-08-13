af2ef0e8bfbf4d7e8198092d942d2fbf4be6ac57
Burt Harris <Burt_Harris.github@33mail.com>
2016-08-13 10:06:46 -0700

    Checkin for tsc error

---
+++ runtime/TypeScript/src/antlr4/error/index.ts
@@ -0,0 +1,5 @@
+export { RecognitionException, NoViableAltException, 
+         LexerNoViableAltException, InputMismatchException, 
+         FailedPredicateException } from './Errors';
+export { DiagnosticErrorListener } from './DiagnosticErrorListener';
+export { ErrorListener, BailErrorStrategy } from './ErrorListener';
